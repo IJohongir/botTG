2ca17773afba5eb9011d363d735228be907de6b7
IJohongir <36514320+IJohongir@users.noreply.github.com>
2022-05-16 15:56:54 +0500

    first commit

---
+++ utils/set_bot_commands.py
@@ -0,0 +1,11 @@
+from aiogram import types
+
+
+async def set_default_commands(dp):
+    await dp.bot.set_my_commands([
+        types.BotCommand("start", "Запустить бота"),
+        types.BotCommand("help", "Помощь"),
+        types.BotCommand("menu","Менью для работы"),
+        types.BotCommand("admin","Для администратора"),
+        types.BotCommand("delete"," Для удаления таблиц")
+    ])
