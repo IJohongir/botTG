744049e211c26616313a377eef23b1f824df0a29
IJohongir <36514320+IJohongir@users.noreply.github.com>
2022-07-19 09:51:04 +0500

    test5

--- handlers/users/Notifiy_work.py
+++ handlers/users/Notifiy_work.py
@@ -199,7 +199,7 @@ async def Stop_user_work(message: types.Message, state: FSMContext):
     data = await state.get_data()
     driver = data.get("driver_id")
     driver_id = int(driver)
-    date = message.date
+    date = message.date.astimezone(tz="Asia/Tashkent")
     view_road = "На работу"
     user_id1 = (data.get("id_Sprint"))
     user_id2 = int(user_id1[1])
